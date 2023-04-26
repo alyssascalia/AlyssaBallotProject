1b00dc36f582442fbec798dc5c249124e02238b2
Alyssa Scalia <a.scalia@setonhill.edu>
2023-04-26 10:38:53 -0400

    ready for review

---
+++ Alyssa Ballot Project/TBViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TBViewController.swift
+//  Alyssa Ballot Project
+//
+//  Created by Alyssa Scalia on 4/23/23.
+//
+
+import UIKit
+
+class TBViewController: UITabBarController {
+
+    var yesVote: Int = 0
+    var noVote: Int = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Alyssa Ballot Project/ViewController.swift
+++ Alyssa Ballot Project/ViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var yesVoteCounter: UILabel!
+    
+    @IBOutlet var noVoteCounter: UILabel!
+    
+    @IBOutlet var winner: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        yesVoteCounter.text = String((parent as! TBViewController).yesVote)
+        noVoteCounter.text = String((parent as! TBViewController).noVote)
+        
+        if (parent as! TBViewController).yesVote > (parent as! TBViewController).noVote
+        {
+            winner.text = "Yes, the campus is pretty"
+        } else {
+            winner.text = "No, we're bias."
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
